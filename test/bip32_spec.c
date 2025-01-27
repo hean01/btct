8e0ffbbca8aadbd49519a315e4d1d58287045092
Henrik Andersson <henrik.4e@gmail.com>
2025-01-27 14:09:50 +0100

    Add test for bip32 root key generation

--- test/bip32_spec.c
+++ test/bip32_spec.c
@@ -35,5 +35,21 @@ spec("bip32") {
       it("then it should succeed")
 	check(bip32_key_init_from_entropy(&key, entropy, sizeof(entropy)) == 0);
     }
+
+  }
+
+  context("Given initialized key from know seed") {
+    before_each() {
+      bip32_key_init_from_entropy(&key, vectors[0].seed, sizeof(vectors[0].seed));
+    }
+
+
+    describe("when extending bip32 root key") {
+      char buffer[1024];
+      size_t size = sizeof(buffer);
+      bip32_key_to_extended_key(&key, true, true, (uint8_t*)buffer, &size);
+      it("should return the correct xprv* string")
+	check_str(buffer, vectors[0].masterkey);
+    }
   }
 }
